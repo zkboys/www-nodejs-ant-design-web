3128443167fd3a6b2b1dc2687ded3d1ca9b3ece6
wangshubin <876213774@163.com>
2015-12-26 23:47:31 +0800

    测试

--- assets/src/contact/contact.jsx
+++ assets/src/contact/contact.jsx
@@ -7,14 +7,13 @@ import App from '../common/App';
 import MyModal from '../common/MyModal.jsx';
 import contactImg from './img/contact.jpg';
 ReactDOM.render(
-    <div className="contact" style={{background:'red',fontSize:'20px'}}>
+    <div className="contact" style={{backgroundColor:'red',fontSize:'20px'}}>
         <img src={contactImg} width="100px"/>
         <App/>
         <MyButton/>
         <MyModal btnText={'联系我们'} title={'提示'} content={'请拨打下面的电话,联系我们.'} okCb={modalOKCB} cancelCb={modalCancelCB}/>
     </div>
     , document.getElementById('react-content'));
-
 function modalOKCB() {
     alert('ok');
 }
