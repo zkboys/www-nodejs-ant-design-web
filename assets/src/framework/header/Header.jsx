74cdfecd28a3238aa223b55b9be57c55648337ff
wangshubin <876213774@163.com>
2016-01-18 22:58:35 +0800

    增加头部系统导航html代码，逻辑还未写。

--- assets/src/framework/header/Header.jsx
+++ assets/src/framework/header/Header.jsx
@@ -1,8 +1,10 @@
 import './style.css';
 import React from 'react';
-import {Icon, Badge, Tooltip} from 'antd';
+import {Icon, Badge, Tooltip ,Menu} from 'antd';
 import {Link} from 'react-router'
 import FAIcon from '../faicon/FAIcon';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 import avatar from './86.jpg';
 var logoMaxWidth = 240
     , logoMinWidth = 60
@@ -10,13 +12,38 @@ var logoMaxWidth = 240
     , logoMin = "后台"
     ;
 const Header = React.createClass({
+    getInitialState(){
+        return {
+            current: 'mail'
+        };
+    },
+    handleClick(e) {
+        console.log('click ', e);
+        this.setState({
+            current: e.key
+        });
+    },
     render() {
         return (
             <header className="admin-header">
-                <div className="admin-logo" style={{width:this.props.collapse?logoMinWidth:logoMaxWidth}}><Link to="/">{this.props.collapse?logoMin:logoMax}</Link></div>
+                <div className="admin-logo" style={{width:this.props.collapse?logoMinWidth:logoMaxWidth}}><Link to="/">{this.props.collapse ? logoMin : logoMax}</Link></div>
                 <Tooltip placement="right" title="切换菜单状态">
                     <a className="admin-sidebar-toggle" onClick={this.props.handelClick}><FAIcon type="fa-bars"/></a>
                 </Tooltip>
+                <Menu className="admin-header-sys" onClick={this.handleClick}
+                      selectedKeys={[this.state.current]}
+                      theme={this.state.theme}
+                      mode="horizontal">
+                    <Menu.Item key="mail">
+                        <Icon type="appstore"/>店铺
+                    </Menu.Item>
+                    <Menu.Item key="app">
+                        <Icon type="appstore"/>服务
+                    </Menu.Item>
+                    <Menu.Item key="alipay">
+                        <Icon type="appstore"/>快递
+                    </Menu.Item>
+                </Menu>
                 <ul className="admin-header-menu">
                     <li className="admin-header-menu-item">
                         <Tooltip placement="bottom" title="我的邮件">
