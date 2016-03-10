77b87aa89e48c3599e0abaeba4e3021321e52074
wangshubin <wangshubin@eking.mobi>
2016-03-10 10:28:01 +0800

    改为按需加载模式

--- assets/src/framework/Routes.jsx
+++ assets/src/framework/Routes.jsx
@@ -2,32 +2,58 @@ import React from 'react';
 import {Router, Link} from 'react-router'
 import {Menu, Tooltip} from 'antd';
 import FAIcon from './faicon/FAIcon';
-import App from './app/App'
-import Home from '../page/home/Home'
-import Error404 from './error/Error404'
-import SettingsPage from './settings/SettingsPage'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 const browserHistory = createBrowserHistory();
 import PubSubMsg from './common/pubsubmsg';
 import {getSidebarMenus, getCurrentSidebarMenu} from './SidebarMenu';
 import {getHeaderMenus} from './HeaderMenu';
-import pageRouts from '../page/RoutesCfg';
+let pageRouts = [
+    {path: '/system/mail/unread', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/mail/UnReadMail'));})}},
+    {path: '/system/mail/read', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/mail/ReadMail'));})}},
+    {path: '/system/remind', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/remind/Remind'));})}},
+    {path: '/system/profile/message', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/profile/ProfileMessage'));})}},
+    {path: '/system/profile/password', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/profile/ProfilePassWord'));})}},
+
+    {path: '/shop/MyForm', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/myform/MyForm'));})}},
+    {path: '/shop/Dashboard', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/dashboard/Dashboard'));})}},
+    {path: '/shop/MyTime', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/mytime/MyTime'));})}},
+    {path: '/shop/ValidationDemo', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/validation-demo/ValidationDemo'));})}},
+
+    {path: '/service/MyForm', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/myform/MyForm'));})}},
+    {path: '/service/Dashboard', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/dashboard/Dashboard'));})}},
+    {path: '/service/MyTime', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/mytime/MyTime'));})}},
+    {path: '/service/ValidationDemo', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/validation-demo/ValidationDemo'));})}},
+
+    {path: '/expressage/MyForm', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/myform/MyForm'));})}},
+    {path: '/expressage/Dashboard', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/dashboard/Dashboard'));})}},
+    {path: '/expressage/MyTime', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/mytime/MyTime'));})}},
+    {path: '/expressage/ValidationDemo', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/validation-demo/ValidationDemo'));})}},
+    {path: '/system/settings', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('./settings/SettingsPage'));})}},
+    {path: '*', getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('./error/Error404'));})}}
+];
 /*
  * 根据菜单数据，初始化路由
  * */
 const routes = {
     path: '/',
-    component: App,
-    indexRoute: {component: Home},
+    component: require('./app/App'),
+    indexRoute: {
+        getComponent: (location, cb) => {require.ensure([], (require) => {cb(null, require('../page/home/Home'));})}
+    },
     childRoutes: pageRouts
 };
 /*
- * 所有未截获的请求,统一跳转到Error404组件
- * */
-routes.childRoutes.push(
-    {path: '/system/settings', component: SettingsPage},
-    {path: '*', component: Error404}
-);
+ for (let i = 0; i < pageRouts.length; i++) {
+ let r = {
+ path: pageRouts[i].path,
+ getComponent: (location, cb) => {
+ require.ensure([], (require) => {
+ cb(null, require(pageRouts[i].component))
+ })
+ }
+ };
+ routes.childRoutes.push(r);
+ }*/
 /*
  * 监听地址栏改变，通过左侧菜单状态
  * */
--- assets/src/page/RoutesCfg.js
+++ assets/src/page/RoutesCfg.js
@@ -8,24 +8,24 @@ import Remind from './remind/Remind'
 import ProfileMessage from './profile/ProfileMessage'
 import ProfilePassWord from './profile/ProfilePassWord'
 export default[
-    {path: '/system/mail/unread', component: UnReadMail},
-    {path: '/system/mail/read', component: ReadMail},
-    {path: '/system/remind', component: Remind},
-    {path: '/system/profile/message', component: ProfileMessage},
-    {path: '/system/profile/password', component: ProfilePassWord},
+    {path: '/system/mail/unread', component: './mail/UnReadMail'},
+    {path: '/system/mail/read', component: './mail/ReadMail'},
+    {path: '/system/remind', component: './remind/Remind'},
+    {path: '/system/profile/message', component: './profile/ProfileMessage'},
+    {path: '/system/profile/password', component: './profile/ProfilePassWord'},
 
-    {path: '/shop/MyForm', component: MyForm},
-    {path: '/shop/Dashboard', component: Dashboard},
-    {path: '/shop/MyTime', component: MyTime},
-    {path: '/shop/ValidationDemo', component: ValidationDemo},
+    {path: '/shop/MyForm', component: './myform/MyForm'},
+    {path: '/shop/Dashboard', component: './dashboard/Dashboard'},
+    {path: '/shop/MyTime', component: './mytime/MyTime'},
+    {path: '/shop/ValidationDemo', component: './validation-demo/ValidationDemo'},
 
-    {path: '/service/MyForm', component: MyForm},
-    {path: '/service/Dashboard', component: Dashboard},
-    {path: '/service/MyTime', component: MyTime},
-    {path: '/service/ValidationDemo', component: ValidationDemo},
+    {path: '/service/MyForm', component: './myform/MyForm'},
+    {path: '/service/Dashboard', component: './dashboard/Dashboard'},
+    {path: '/service/MyTime', component: './mytime/MyTime'},
+    {path: '/service/ValidationDemo', component: './validation-demo/ValidationDemo'},
 
-    {path: '/expressage/MyForm', component: MyForm},
-    {path: '/expressage/Dashboard', component: Dashboard},
-    {path: '/expressage/MyTime', component: MyTime},
-    {path: '/expressage/ValidationDemo', component: ValidationDemo}
+    {path: '/expressage/MyForm', component: './myform/MyForm'},
+    {path: '/expressage/Dashboard', component: './dashboard/Dashboard'},
+    {path: '/expressage/MyTime', component: './mytime/MyTime'},
+    {path: '/expressage/ValidationDemo', component: './validation-demo/ValidationDemo'}
 ]
