e02e39cc8c8dd3f05bf4820039d416f5f519db12
wangshubin <wangshubin@xiaoyage.com>
2016-03-25 16:17:56 +0800

    修改构建流程 改用兼容写法

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,6 +2,7 @@ var join = require("path").join;
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var webpack = require('webpack');
 var child_process = require('child_process');
+var rummod = process.env.runmod || 'development';
 /*
  * 基于不同模式，区分配置
  * */
@@ -24,40 +25,11 @@ var configs = {
     }
 };
 
-/*
- * 构建之前是否清除之前构建的内容。
- * */
-var clean = true;
-if (process.env.CLEAN == 'false') {
-    clean = false;
-}
+
 /*
  * 获取不同的环境配置
  * */
-var cfg = configs.development;
-var env = process.env.RUNMODE;
-switch (env) {
-    case 'dev':
-    {
-        cfg = configs.development;
-        break;
-    }
-    case 'test':
-    {
-        cfg = configs.test;
-        break;
-    }
-    case 'pro':
-    {
-        cfg = configs.production;
-        break;
-    }
-    case 'devserver':
-    {
-        cfg = configs.devserver;
-        break;
-    }
-}
+var cfg = configs[rummod] || configs.development;
 /*
  * 定义entry
  * 如果项目结构命名有良好的约定，是否考虑使用代码自动生成entry？
@@ -67,20 +39,6 @@ var _entry = {
     "index": './src/entry/index.jsx'
 };
 
-/*
- * 构建之前，先清除之前构建的内容
- * 注：这样有个问题，执行 webpack-dev-server 的时候会删除对应的构建内容，并且没有再生成。
- * */
-if (clean) {
-    var targetDir = join(__dirname, cfg.path);
-    //var shell = 'rm -rf ' + targetDir + '&& mkdir ' + targetDir + "&& cp index.html "+ targetDir;
-    var shell = 'rm -rf ' + targetDir;
-    child_process.exec(shell, function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    });
-}
 /*
  *　Routes转化
  * */
