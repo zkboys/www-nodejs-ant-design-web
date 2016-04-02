de78acbd73fa0f139b60167c35014b23a062c625
wangshubin <876213774@qq.com>
2016-04-02 08:37:59 +0800

    列表页

--- assets/src/page/list-page/ListPage.jsx
+++ assets/src/page/list-page/ListPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Breadcrumb } from 'antd'
+import { Breadcrumb, Select } from 'antd'
 import Page from '../../framework/page/Page';
+const Option = Select.Option;
 class ListPage extends React.Component {
     constructor(props) {
         super(props);
@@ -13,7 +14,28 @@ class ListPage extends React.Component {
     render() {
         return (
             <Page header="auto" loading={this.state.loading}>
-                <h1>我是列表页</h1>
+                <div className="search-area">
+                    <Select showSearch
+                            style={{ width: 200 }}
+                            placeholder="请选择人员"
+                            optionFilterProp="children"
+                            notFoundContent="无法找到"
+                            searchPlaceholder="输入关键词"
+                            onChange={handleChange}>
+                        <Option value="jack">杰克</Option>
+                        <Option value="lucy">露西</Option>
+                        <Option value="tom">汤姆</Option>
+                    </Select>
+                </div>
+                <div className="top-tool-bar">
+                    顶部工具条
+                </div>
+                <div className="data-table">
+                    表格
+                </div>
+                <div className="bottom-tool-bar">
+                    底部工具条
+                </div>
             </Page>
         );
     }
